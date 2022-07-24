9c867c75c820f877a13e78b8c624dab8423d24f8
AYA-ELNAGAR <83846941+AYA-ELNAGAR@users.noreply.github.com>
2022-07-24 17:02:48 +0200

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,40 @@
+//qq1
+//  let ara=[1,2,3,4,5];
+// let [a,b,c,d,e]=ara;
+// console.log(a*e);
+
+
+// --------------------
+
+// q2
+
+// const member={
+//     age:30,
+//     working:false,
+//     country:"egypt",
+//     hoopies:["reading","swimming","programming"]
+
+
+// }
+// let age=member.age;
+// let working=member.working;
+
+// let country=member.country;
+
+// let hoopies=member.hoopies;
+// ({age:a,working:w,country:c,hoopies:h}=member);
+// console.log(`my age is ${a} and iam ${w ? " " : "Not" } working`);
+// console.log(`ilive in ${c}`)
+// console.log(`my hoppy is${h[0]} and ${h[2]}`)
+// ---------------------
+
+// QQ3
+
+document.getElementById("b1").onclick=function(){
+document.getElementById("g1").style.display="none"
+    
+}
+document.getElementById("b2").onclick=function(){
+    document.getElementById("g1").style.display="block"
+        
+    }
